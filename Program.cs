5fb2140256b26ecabd52adb819b9b31f0f76c667
Nicolas Robert <nicolas.robert@cellenza.com>
2024-11-03 22:13:21 +0100

    Demo init

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using Azure.AI.OpenAI;
+using Azure.Identity;
+
+Console.WriteLine("Hello, DevDayBE!");
+
+// Call Azure OpenAI using Credentials: will use Managed Identity or Azure CLI
+var tokenCredential = new ChainedTokenCredential(new ManagedIdentityCredential(), new AzureCliCredential());
+var client = new AzureOpenAIClient(new Uri("https://oai-demo-identity-swc.openai.azure.com/"), tokenCredential);
+
+// Generate an image using the DALL-E model
+var generatedImageResult = await client.GetImageClient("dall-e-3").GenerateImageAsync("A giant belgian waffle in a beer glass shape, pixel art style");
+var image = generatedImageResult.Value;
+
+// Save the image to a file
+var imageFileName = $"waffle-{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.png";
+using var httpClient = new HttpClient();
+using var s = await httpClient.GetStreamAsync(image.ImageUri);
+using var fs = new FileStream(imageFileName, FileMode.OpenOrCreate);
+await s.CopyToAsync(fs);
+Console.WriteLine($"Image saved to '{imageFileName}'");
+
+// Display the image using the default image viewer
+Process.Start(new ProcessStartInfo(imageFileName) { UseShellExecute = true });
---
+++ README.md
@@ -0,0 +1,37 @@
+# AzureCredentialsDemo
+
+This project demonstrates how to use Azure services with managed identity or Azure CLI credentials.
+
+In this demo, I am using consuming an Azure OpenAI resource to generate images using the DALL-E model.
+
+## Prerequisites
+
+- .NET 8.0 SDK
+- An Azure subscription with an Azure OpenAI resource created
+- Permission set to the Azure OpenAI resource (here I am using "Cognitive Services OpenAI User")
+- A "dall-e 3" model deployed within this Azure OpenAI resource (this deployment is named "dall-e-3" here in the code)
+
+## Running the Project
+
+Don't forget to Connect to your Azure account (the one having the appropriate permission on the Azure OpenAI resource):
+```sh
+az login
+```
+
+To run the project, use the following command:
+```sh
+dotnet run
+```
+
+## Debug
+You can debug the project using Visual Studio Code:
+```sh
+code .
+```
+Then, press F5 to start debugging.
+
+## Additional information
+To check which Azure account is currently connected, use the following command:
+```sh
+az account show
+```
